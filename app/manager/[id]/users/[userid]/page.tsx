e7515bf083048b80c889e4c431245918ecb032d1
ishimwebernard <isbernard2001@gmail.com>
2025-02-16 15:38:15 +0200

    mend

---
+++ app/manager/[id]/users/[userid]/page.tsx
@@ -0,0 +1,252 @@
+"use client"
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
+import { AppSidebar } from "@/components/manager-sidebar"
+import { usePathname } from "next/navigation"
+//import { useRouter } from "next/router";
+import {useEffect, useState} from "react"
+import axios from "axios"
+import {
+    Table,
+    TableBody,
+    TableCaption,
+    TableCell,
+    TableFooter,
+    TableHead,
+    TableHeader,
+    TableRow,
+  } from "@/components/ui/table"
+  import { Button } from "@/components/ui/button"
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import { Textarea } from "@/components/ui/textarea"
+import { toast } from "sonner"
+import { WarningProvider } from "@radix-ui/react-dialog"
+import {
+    Select,
+    SelectContent,
+    SelectGroup,
+    SelectItem,
+    SelectLabel,
+    SelectTrigger,
+    SelectValue,
+  } from "@/components/ui/select"
+  import {
+    Tabs,
+    TabsContent,
+    TabsList,
+    TabsTrigger,
+  } from "@/components/ui/tabs"
+
+
+export default function Shopkeeper(){
+    const lePath = usePathname().split('/')[2]
+    //const router = useRouter()
+    const [stockGraphics, setStockGraphics] = useState([])
+    const [shopkeepers, setShopkeepers] = useState([])
+
+    let editedItem = {
+        name: '',
+        quantity: 0,
+        costprice: 0,
+        price: 0,
+        picture: '',
+        description: ''
+    }
+
+    let newItem =  { shopkeeperId: 0, 
+      name: '', 
+      quantity: 0, 
+      price: 0, 
+      picture: '', 
+      description: '', 
+      costprice: 0 
+    }
+
+
+    useEffect(()=>{
+        async function fetchStock(){
+            const tempStock = []
+            const shopExistingStock = await axios({
+                method: 'get',
+                url: 'http://localhost:3000/getstockitems'
+            })
+            shopExistingStock.data.forEach((item)=>{
+                tempStock.push(
+                    <TableRow>
+                    <TableCell>{item.id}</TableCell>
+                    <TableCell>{item.shopkeeperId}</TableCell>
+                    <TableCell>{item.name}</TableCell>
+                    <TableCell>{item.quantity}</TableCell>
+                    <TableCell>{item.price}</TableCell>
+                    <TableCell>{item.costprice}</TableCell>
+                    <TableCell>
+                    <Dialog>
+      <DialogTrigger asChild>
+        <Button variant="outline" onClick={()=>{
+            editedItem = {name:item.name, quantity:item.quantity, price:item.price, costprice:item.costprice, picture:item.picture, description:item.description}
+            console.log(editedItem)
+        }}>Edit</Button>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>Update records</DialogTitle>
+          <DialogDescription>
+            Here, you can make changes so as to update the stock, remove the item if it is no longer sold, and updating the prices
+          </DialogDescription>
+        </DialogHeader>
+        <div className="grid gap-4 py-4">
+          <div className="grid grid-cols-4 items-center gap-4">
+  <Label htmlFor="name" className="text-right">
+    Name
+  </Label>
+  <Input id="name" defaultValue={item.name} className="col-span-3" onChange={(e)=>{
+    editedItem = {...editedItem, name: e.target.value}
+  }}/>
+</div>
+
+<div className="grid grid-cols-4 items-center gap-4">
+  <Label htmlFor="quantity" className="text-right">
+    Quantity
+  </Label>
+  <Input id="quantity" defaultValue={item.quantity} className="col-span-3" onChange={(e)=>{
+    editedItem = {...editedItem, quantity: parseInt(e.target.value, 10)}
+  }}/>
+</div>
+
+<div className="grid grid-cols-4 items-center gap-4">
+  <Label htmlFor="price" className="text-right">
+    Price ($)
+  </Label>
+  <Input id="price" defaultValue={item.price} className="col-span-3" onChange={(e)=>{
+    editedItem = {...editedItem, price: parseInt(e.target.value, 10)}
+  }}/>
+</div>
+
+<div className="grid grid-cols-4 items-center gap-4">
+  <Label htmlFor="costprice" className="text-right">
+    Cost Price ($)
+  </Label>
+  <Input id="costprice" defaultValue={item.costprice} className="col-span-3" onChange={(e)=>{
+    editedItem = {...editedItem, costprice: parseInt(e.target.value, 10)}
+  }}/>
+</div>
+
+<div className="grid grid-cols-4 items-center gap-4">
+  <Label htmlFor="description" className="text-right">
+    Description
+  </Label>
+  <Textarea id="description" defaultValue={item.description} className="col-span-3" onChange={(e)=>{
+    editedItem = {...editedItem, description: e.target.value}
+  }}/>
+</div>
+
+<div className="grid grid-cols-4 items-center gap-4">
+  <Label htmlFor="picture" className="text-right">
+    Picture
+  </Label>
+  <Input id="picture" defaultValue={item.picture} className="col-span-3" onChange={(e)=>{
+    editedItem = {...editedItem, picture: e.target.value}
+  }}/>
+</div>
+
+          
+
+        </div>
+        <DialogFooter>
+          <Button type="submit" onClick={async()=>{
+            console.log(editedItem)
+            try{
+                const res = await axios({
+                    method: 'put',
+                    url: 'http://localhost:3000/stockitems/'+item.id,
+                    data: editedItem
+                })
+                toast("Item Updated Succesfully", {
+                    description: item.name + " was succesfully updated",
+                    action: {
+                      label: "Ok",
+                      onClick: () => console.log("Undo"),
+                    },
+                  })
+                window.location.reload()
+                  
+            }catch(error){
+                toast("Error", {description: "Something went wrong!"})
+            }
+            
+
+          }}>Save changes</Button>
+          <Button onClick={async()=>{
+            try{
+                const res = await axios({
+                    method: 'delete',
+                    url: 'http://localhost:3000/stockitems/'+item.id
+                })
+                toast("Delete Succesful", {description: item.name + " was deleted succesfully!"})
+                window.location.reload()
+
+            }catch(error){
+                toast("Error", {description: "Something went wrong!"})
+            }
+          }}>Delete</Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+                    </TableCell>
+                </TableRow>
+                )
+            })
+
+            console.log(tempStock)
+            setStockGraphics(tempStock)
+        }
+
+        async function findShopkeepers(){
+            try{
+                let tempArray = []
+                const shopkeepers = await axios({
+                    method: 'get',
+                    url: 'http://localhost:3000/shopkeepers',
+                })
+                shopkeepers.data.forEach((item: any)=>{
+                    tempArray.push(<option value={item.id}>{item.name}</option>)
+                })
+                setShopkeepers(tempArray)
+
+            }catch(error){
+                toast("error", {description: "Something went wrong"})
+            }
+        }
+        fetchStock()
+        findShopkeepers()
+    }, [])
+    return (
+        <SidebarProvider>
+        <AppSidebar />
+        <main>
+          <SidebarTrigger />
+        <Tabs>
+            <TabsList>
+                <TabsTrigger value="customers">Customers</TabsTrigger>
+                <TabsTrigger value="shopkeepers">Shopkeepers</TabsTrigger>
+            </TabsList>
+            <TabsContent value="customers">
+                <p>Customers</p>
+            </TabsContent>
+            <TabsContent value="shopkeepers">
+                <p>Shopkeepers</p>
+            </TabsContent>
+        </Tabs>
+        </main>
+      </SidebarProvider>
+    )
+}
--- app/manager/[id]/users/page.tsx
+++ app/manager/[id]/users/page.tsx
@@ -1,30 +1,186 @@
 "use client"
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
 import { AppSidebar } from "@/components/manager-sidebar"
-import {useEffect} from "react"
+import { usePathname } from "next/navigation"
+//import { useRouter } from "next/router";
+import {useEffect, useState} from "react"
+import axios from "axios"
+import {
+    Table,
+    TableBody,
+    TableCaption,
+    TableCell,
+    TableFooter,
+    TableHead,
+    TableHeader,
+    TableRow,
+  } from "@/components/ui/table"
+  import { Button } from "@/components/ui/button"
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import { Textarea } from "@/components/ui/textarea"
+import { toast } from "sonner"
+import { WarningProvider } from "@radix-ui/react-dialog"
+import {
+    Select,
+    SelectContent,
+    SelectGroup,
+    SelectItem,
+    SelectLabel,
+    SelectTrigger,
+    SelectValue,
+  } from "@/components/ui/select"
+  import {
+    Tabs,
+    TabsContent,
+    TabsList,
+    TabsTrigger,
+  } from "@/components/ui/tabs"
+  import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+  } from "@/components/ui/card"
 
-export default function Manager({params}:any) {
+export default function Shopkeeper(){
+    const lePath = usePathname().split('/')[2]
+    //const router = useRouter()
+    const [customerGraphics, setCustomerGraphics] = useState([])
+    const [shopkeepers, setShopkeepers] = useState([])
 
-  return (
-    <SidebarProvider>
-      <AppSidebar />
-      <main>
-        <SidebarTrigger />
-        <div>
-            <p>This is it</p>
-            <p>{params.id}</p>
+
+    useEffect(()=>{
+        async function fetchStock(){
+            const tempCustomers = []
+            const customers = await axios({
+                method: 'get',
+                url: 'http://localhost:3000/customers'
+            })
+            customers.data.forEach((item)=>{
+              tempCustomers.push(
+                <TableRow>
+                <TableCell>{item.id}</TableCell>
+                 <TableCell>{item.name}</TableCell>
+                 <TableCell>{item.email}</TableCell>
+                 <TableCell>{item.phonenumber}</TableCell>
+                 <TableCell>{item.customerClass}</TableCell>
+                 <TableCell>{item.referrerId}</TableCell>
+                 <TableCell>Rwf {item.pfr}</TableCell>
+                 <TableCell>{item.percentagetogive} %</TableCell>
+                </TableRow>
+              )
+            })
+
+            setCustomerGraphics(tempCustomers)
+        }
+
+        async function findShopkeepers(){
+            try{
+                let tempArray = []
+                const shopkeepers = await axios({
+                    method: 'get',
+                    url: 'http://localhost:3000/shopkeepers',
+                })
+                shopkeepers.data.forEach((item: any)=>{
+                    tempArray.push(
+                      <TableRow>
+                      <TableCell>{item.id}</TableCell>
+                      <TableCell>{item.name}</TableCell>
+                      <TableCell>{item.email}</TableCell>
+                      <TableCell>{item.phonenumber}</TableCell>
+                      <TableCell>{item.managerId}</TableCell>
+                      </TableRow>
+                    )
+                })
+                setShopkeepers(tempArray)
+
+            }catch(error){
+                toast("error", {description: "Something went wrong"})
+            }
+        }
+        fetchStock()
+        findShopkeepers()
+    }, [])
+    return (
+        <SidebarProvider>
+        <AppSidebar />
+        <main className="w-full">
+          <SidebarTrigger />
+       <div className="p-8">
+       <Tabs defaultValue="customers" className="">
+            <TabsList className="grid w-full grid-cols-2">
+                <TabsTrigger value="customers">Customers</TabsTrigger>
+                <TabsTrigger value="shopkeepers">Shopkeepers</TabsTrigger>
+            </TabsList>
+            <TabsContent value="customers" className="">
+              <Card className="p-4">
+                <CardTitle>
+                  Active Customers
+                </CardTitle>
+                <CardDescription>
+                  The table below shows the customers you have as well as their profits, classes and other relevant information.
+                  You cannot change this in any way.
+                </CardDescription>
+                <CardContent>
+                <Table>
+                  <TableHeader>
+                   <TableRow>
+                   <TableHead>Id</TableHead>
+                    <TableHead>Name</TableHead>
+                    <TableHead>Email</TableHead>
+                    <TableHead>Phone Number</TableHead>
+                    <TableHead>Class</TableHead>
+                    <TableHead>Referred By</TableHead>
+                    <TableHead>Profit Accumulated</TableHead>
+                    <TableHead>Percentage to distribute</TableHead>
+                   </TableRow>
+                  </TableHeader>
+                  <TableBody>
+                      {customerGraphics}
+                  </TableBody>
+                </Table>
+                </CardContent>
+              </Card>
+            </TabsContent>
+            <TabsContent value="shopkeepers">
+                <Card className="p-4">
+                  <CardTitle>Shopkeeper</CardTitle>
+                  <CardDescription>
+                    View Shopkeepers as well as their details. Note that the details are not editable, edits could be made from the shopkeeper's portal
+                  </CardDescription>
+                  <CardContent>
+                    <Table>
+                      <TableHeader>
+                        <TableRow>
+                        <TableHead>ID</TableHead>
+                        <TableHead>Name</TableHead>
+                        <TableHead>Email</TableHead>
+                        <TableHead>Phone Number</TableHead>
+                        <TableHead>Manager ID</TableHead>
+                        </TableRow>
+                      </TableHeader>
+                      <TableBody>
+                        {shopkeepers}
+                      </TableBody>
+                    </Table>
+                  </CardContent>
+                </Card>
+            </TabsContent>
+        </Tabs>
         </div>
-      </main>
-    </SidebarProvider>
-  )
+        </main>
+      </SidebarProvider>
+    )
 }
-
-
-// export default function Customer({params}:any){
-
-//     return (
-// <div>
-//     <p>{params.id}</p>
-// </div>
-//     )
-// }
